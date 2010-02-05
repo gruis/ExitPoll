16748fe94ecd9eb1924f4a80c2fdf4b216124272
Caleb Crane <github@simualcre.org>
2010-02-06 00:43:37 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+ExitPoll
+==================
+
+ExitPoll uses Google Analytics to keep track when a user leaves your site, or downloads a file. It works with either the synchronous, or asynchronous Google Analytics tracking.
+
+![Screenshot](http://c5.simulacre.org/exitpoll/images/banner_200x100.png)
+
+
+How to use
+----------
+
+1. Install the Google Analytics tracking code as usual. The asynchronous and synchronous versions are both supported.
+2. Instantiate an ExitPoll object once the DOM is ready.
+    
+
+    window.addEvent("domready", function(){
+       new ExitPoll(); 
+    });
+
+ExitPoll will then bind mouseup events on all links that point to another server, different port on the same server, or non-html file on the same server. Mailto links will also be caught. When the user clicks on one of the links, and does not cancel the click, ExitPoll will register a PageView with Google Analytics. Outgoing links will be prepended with "/outgoing/". Download links will be prepended with "/download/".
+
+Syntax
+-----
+
+    new ExitPoll([options]);
+    
+Arguments
+---------
+
+	1. options - (object, optional) the options described below:
+
+Options
+-------
+* parent        : (element) An element that is the parent of all the links that you want to track. Defaults to document.body.
+* fileTypes     : (array) The list of file extensions that should be tracked as downloadable files. Defaults to: [ "pdf", "zip", "rar", "tgz", "gz","gzip", "jpg", "png", "svg", "gif", "doc", "eps", "xls", "ppt", "xls", "txt", "vsd", "js", "css", "rar", "exe", "wma", "mov", "avi", "wmv", "mp3"]
---
+++ Source/exitpoll-nc.js
@@ -0,0 +1,64 @@
+/*
+---
+description:   ExitPoll uses Google Analytics to keep track when a user leaves your site, or downloads a file. It works with either the synchronous, or asynchronous Google Analytics tracking.
+ 
+license: 
+   - MIT-style
+ 
+authors:
+   - Caleb Crane (http://www.simulacre.org)
+ 
+requires: 
+  core/*:
+ 
+provides: 
+   - ExitPoll
+ 
+...
+*/
+var ExitPoll = new Class({
+    Implement: [Options],
+    options: {
+        parent    : null,
+        fileTypes : [ "pdf", "zip", "rar", "tgz", "gz","gzip", "jpg", "png", "svg", "gif", "doc", "eps", "xls", "ppt", "xls", "txt", "vsd", "js", "css", "rar", "exe", "wma", "mov", "avi", "wmv", "mp3"]
+    },
+    initialize: function(options){
+        var my = this;
+        var par = my.options.parent == null ? document.body : my.options.parent;
+        
+        $(par).getElements('a').each(function(a){
+            if(a.protocol != "mailto:" && (!$chk(a.href) || a.href == "#")) return;
+            
+            if(a.host == location.host){
+                if(!my.options.fileTypes.contains( a.pathname.substring(a.pathname.lastIndexOf(".")+1).toLowerCase() ))
+                    return
+            } 
+
+            a.addEvent("mouseup", my.track ); // use mouseup in order to catch middle click too; if we use mousedown then canceled clicks will also be counted
+
+        });
+    }, 
+    track: function(){
+        var l;
+
+        if(this.protocol == "mailto:")      l = this.href;
+        else if(this.host != location.host) l = "/outgoing/" + this.href;
+        else                                l = "/download" + this.pathname;
+        
+        if(!$chk(l)) return;
+        
+        dbug.log("l:" + l);
+    
+        if (typeof(pageTracker) == "object") pageTracker._trackPageview(l);
+        else if(typeof(_gaq) == "object") _gaq.push(['_trackPageview', l]);
+        /*else throw("Google Analytics tracking object not found");*/ // uncomment to enable throwing or errors;
+
+        return;
+    }
+});
+
+
+
+
+
+
